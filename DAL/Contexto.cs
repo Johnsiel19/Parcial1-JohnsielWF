6efbffb96e35609903a3efba0d98ee56a5c7d573
Johnsiel Antonio Castanos Hernandez <johnsiel_castanos@ucne.edu.do>
2019-09-24 23:20:40 -0400

    Agragando IRepositorio, Repositorio Base y Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+
+
+       // public DbSet<Pagos> Pagos { get; set; }
+
+
+
+        public Contexto() : base("ConStr")
+        { }
+
+    }
+}
